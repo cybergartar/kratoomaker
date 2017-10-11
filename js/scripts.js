420783a0259ecbbe0689dffe6425a01094ba995f
cybergartar <kamonnop.a@gmail.com>
2017-10-11 18:43:14 +0700

    [ADD] Add web files

---
+++ js/scripts.js
@@ -0,0 +1,26 @@
+$( document ).ready(function() {
+    console.log( "ready!" );
+    var dte = new Date();
+    var day = dte.getDate();
+    var month = dte.getMonth();
+    var year = dte.getYear() + 1990 + 543;
+    var hr = dte.getHours();
+    var min = dte.getMinutes();
+    var sec = dte.getSeconds();
+
+    var mon_thai = ["มกราคม", "กุมภาพันธ์", "มีนาคม", "เมษายน", "พฤษภาคม", "มิถุนายน", "กรกฎาคม", "สิงหาคม", "กันยายน", "ตุลาคม", "พฤศจิกายน", "ธันวาคม"];
+
+    console.log("" + day + " " + month + " " + year + " " + hr + " " + min + " " + sec);
+
+    document.getElementById('datetopic').innerHTML = "" + day + " " + mon_thai[month] + " " + year + " เวลา  " + hr + ":" + min + ":" + sec + " น.";
+});
+
+document.getElementById('tags').onkeyup = function() {
+    var str = document.getElementById('tags').value;
+    var tags = str.trim().split(" ")
+    var inner = ""
+    for (i in tags) {
+        inner = inner + "<a class=\"button tiny\">" + tags[i] + "</a>&nbsp;";
+    }
+    document.getElementById('tagDisplay').innerHTML = inner;
+}
